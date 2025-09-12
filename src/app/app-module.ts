715cbbd51d0bdd8f743726f27814d49e5ff52a1f
Camila Sousa <camilamariano10@gmail.com>
2025-09-11 23:05:10 -0400

    Finalização da navbar

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './core/header/header';
 import { Footer } from './core/footer/footer';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Footer } from './core/footer/footer';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
