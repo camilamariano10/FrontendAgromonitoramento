3ef3425c008c5cbc3fdfabc7ec82220baf79d3b3
leona <silvarodrigues456@gmail.com>
2025-09-14 01:43:53 -0400

    Implementação do modal de login

---
+++ src/app/core/header/header.ts
@@ -0,0 +1,22 @@
+import { Component, inject, signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Login } from '../../pages/login/login';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.html',
+  styleUrls: ['./header.css'],
+})
+export class Header {
+  protected readonly title = signal('FrontendAgromonitoramento');
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    this.dialog.open(Login, {
+      width: '448px',
+      height: '694px',
+      panelClass: 'custom-modal',
+    });
+  }
+}
